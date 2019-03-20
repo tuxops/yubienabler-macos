18e6b62cc1f40fbf780bedd0fab17724bd8f85f1
Hal Halvorsen <37691775+tuxops@users.noreply.github.com>
2019-03-20 19:09:31 +0000

    Create yubi.sh

---
+++ yubi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# set up system to use YubiKey
+# use setupuser for setting up a new user 
+
+datestamp=$(date +%Y%m%dT%H%M%S)
+sudo cp -p /etc/pam.d/screensaver /etc/pam.d/screensaver.$datestamp
+sudo cp -p /etc/pam.d/authorization /etc/pam.d/authorization.$datestamp
+sudo cp ~/Build/screensaver /etc/pam.d/screensaver
+sudo cp ~/Build/authorization /etc/pam.d/authorization
+sudo chown root /etc/pam.d/screensaver /etc/pam.d/authorization
+sudo chgrp wheel /etc/pam.d/screensaver /etc/pam.d/authorization
+sudo chmod 644 /etc/pam.d/screensaver /etc/pam.d/authorization
