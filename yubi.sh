f2f12e56790f886cda82031fff1e052a0ae87b18
Hal Halvorsen <37691775+tuxops@users.noreply.github.com>
2019-03-29 18:58:22 +0000

    sync for home add

--- yubi.sh
+++ yubi.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
-# set up MacOS to use YubiKey
-# use setupuser for setting up a new user 
+#
+# set up MacOS PAM to use YubiKey
+# using sudo to modify system files, so a MacOS "Admin" account is needed to run
+#
+readonly PAMDIR=/Users/hal/etcpam
+readonly AUTHFILE=${PAMDIR}/authorization
+readonly SCREENSAVERFILE=${PAMDIR}/screensaver
+readonly YUBILIB=/usr/local/lib/security/pam_yubico.so
+readonly DATESTAMP=$(date +%Y%m%dT%H%M%S)
 
-datestamp=$(date +%Y%m%dT%H%M%S)
-sudo cp -p /etc/pam.d/screensaver /etc/pam.d/screensaver.$datestamp
-sudo cp -p /etc/pam.d/authorization /etc/pam.d/authorization.$datestamp
-sudo cp ~/Build/screensaver /etc/pam.d/screensaver
-sudo cp ~/Build/authorization /etc/pam.d/authorization
-sudo chown root /etc/pam.d/screensaver /etc/pam.d/authorization
-sudo chgrp wheel /etc/pam.d/screensaver /etc/pam.d/authorization
-sudo chmod 644 /etc/pam.d/screensaver /etc/pam.d/authorization
+for myfile in $AUTHFILE $SCREENSAVERFILE; do
+  if [[ ! -f $myfile ]]; then
+      echo "WARNING: $myfile does not seem to exist, skipping with no changes made"
+  else
+    # ignore all comment lines, then check for existence of YubiKey library already configured
+    if $(grep -v ^[[:blank:]]*# $myfile | grep -q $YUBILIB 2>/dev/null); then
+      echo "INFO: PAM setup for YubiKey in $myfile looks to already be in place"
+    else
+      echo "INFO adding PAM setup for YubiKey in $myfile"
+      # create a backup
+      sudo cp -p $myfile ${myfile}.$DATESTAMP
+      
+      sudo chown root $myfile
+      sudo chgrp wheel $myfile
+      sudo chmod 644 $myfile
+    fi
+  fi
+done
+
+  
