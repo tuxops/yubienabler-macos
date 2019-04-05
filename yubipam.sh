501912738742dd646db46c40d85277c2ae3595fd
Hal Halvorsen <37691775+tuxops@users.noreply.github.com>
2019-04-05 19:35:51 +0100

    removed code for copying files, instead use sed

--- yubipam.sh
+++ yubipam.sh
@@ -4,17 +4,23 @@
 # uses sudo to modify system files, so a MacOS "Admin" account is needed to run
 #
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
-readonly PAMDIR=/etc/pam.d
+
+readonly PAMDIR=/Users/hal/etcpam
 readonly AUTHFILE=authorization
 readonly SCREENSAVERFILE=screensaver
 readonly YUBILIB=/usr/local/lib/security/pam_yubico.so
 readonly DATESTAMP=$(date +%Y%m%dT%H%M%S)
 readonly INSTALLDIR=$(pwd)
 readonly THISONE=$(basename $0)
+readonly MYTMPDIR=/tmp/tempdir.$RANDOM
+readonly PAMLINE=\
+"auth       required       \
+/usr/local/lib/security/pam_yubico.so mode=challenge-response"
+
 changes=0
 
 echo "INFO: $THISONE checking sudo access"
-if ! $(sudo -v); then
+if ! $(sudo mkdir $MYTMPDIR); then
   echo "ERROR: $THISONE no sudo access, giving up" >&2
   exit 1
 fi
@@ -34,14 +40,22 @@ for myfile in $AUTHFILE $SCREENSAVERFILE; do
      "in $myfile looks to already be in place"
   else
     echo "INFO: $THISONE adding PAM setup for YubiKey in $myfile"
-    # create a backup and copy YubiKey enabled file in place
-    # TODO(tuxops): Use (MacOS=POSIX) sed/awk instead of copying in new file
     sudo cp -p ${PAMDIR}/$myfile ${PAMDIR}/${myfile}.$DATESTAMP
-    sudo cp ${INSTALLDIR}/${myfile}.yubi ${PAMDIR}/$myfile
-    sudo chown root ${PAMDIR}/$myfile
-    sudo chgrp wheel ${PAMDIR}/$myfile
-    sudo chmod 644 ${PAMDIR}/$myfile
-    changes=$((changes+1))
+    # enable YubiKey
+    # sed -e used here to get around newline issue on macOS sed
+    # sed -e '/account.*required.*pam_opendirectory.so/i\'$'\n''newline is here' screensaver
+    mkdir $MYTMPDIR
+    sed -e '/account.*required.*pam_opendirectory.so/i\'$'\n''mynewline is this' ${PAMDIR}/$myfile >${MYTMPDIR}/$myfile
+    if $(sudo cp ${MYTMPDIR}/$myfile ${PAMDIR}/$myfile 2>/dev/null);then
+      sudo chown root ${PAMDIR}/$myfile
+      sudo chgrp wheel ${PAMDIR}/$myfile
+      sudo chmod 644 ${PAMDIR}/$myfile
+      changes=$((changes+1))
+      rm ${MYTMPDIR}/$myfile
+      rmdir $MYTMPDIR
+    else 
+      echo "ERROR: $THISONE could not update ${PAMDIR}/$myfile, giving up" >&2"
+      exit 3
   fi
 done
 
