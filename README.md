e15b134b5f395a220e00e22ab6f2645584714a13
Hal Halvorsen <37691775+tuxops@users.noreply.github.com>
2019-04-09 19:47:21 +0100

    improvements using shellcheck and md file lint

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
+
 # yubienabler-macos #
-A small project to enable the use of YubiKeys on MacOS.
+
+A small project to enable the use of YubiKeys on macOS.
 
 ## Using ##
-Clone or download the project and run yubipam.sh.
+
+Clone or download the project and execute yubipam.sh.
 
 ## Prerequisites ##
-* Yubico PAM for MacOS software
+
+* read the Yubico macOS Logon Tool Configuration Guide before running
+* Yubico PAM for macOS software (aka macOS Logon Tool)
+* a YubiKey that has been initialised
 * you will need to run scripts as a MacOS user with 'Admin' (sudo) privileges
-* /etc/pam.d/authorization and screensaver as originally distributed
+
 ## Files ##
 
 File | Purpose
 ---- | -------
 authorization.dist| authorization file as distributed in MacOS, for recovery
-authorization.yubi| authorization file with Yubico PAM enabled
 screensaver.dist| screensaver file as distributed in MacOS, for recovery
-screensaver.yubi| screensaver file with Yubico PAM enabled
 yubipam.sh|systemwide setup for use of YubiKeys with PAM,
 .|run after install of YubiKey software or after MacOS upgrades
 README.md|this file
-
--- yubipam.sh
+++ yubipam.sh
@@ -5,68 +5,70 @@
 #
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-readonly PAMDIR=/etc/pam.d
+# readonly PAMDIR=/etc/pam.d
+readonly PAMDIR=/Users/hal/etcpam
 readonly AUTHFILE=authorization
 readonly SCREENSAVERFILE=screensaver
 readonly YUBILIB=/usr/local/lib/security/pam_yubico.so
 readonly DATESTAMP=$(date +%Y%m%dT%H%M%S)
 readonly INSTALLDIR=$(pwd)
-readonly THISONE=$(basename $0)
+readonly THISONE=$(basename "$0")
 readonly MYTMPDIR=/tmp/tempdir.$RANDOM
 
-# sticking to 80 character line length...
+# sticking to 80 character line length ...
 readonly PAM1="auth       required       "
 readonly PAM2="/usr/local/lib/security/pam_yubico.so mode=challenge-response"
-readonly PAMLINE="$PAM1""$PAM2"
+readonly PAMLINE="${PAM1}""${PAM2}"
 
 changes=0
 
-echo "INFO: $THISONE checking sudo access"
-if ! $(sudo -v); then
-  echo "ERROR: $THISONE no sudo access, giving up" >&2
+echo "INFO: ${THISONE} checking sudo access"
+if ! sudo -v; then
+  echo "ERROR: ${THISONE} no sudo access, giving up" >&2
   exit 1
 fi
 
-for myfile in $AUTHFILE $SCREENSAVERFILE; do
-  if [[ ! -f ${PAMDIR}/$myfile ]]; then
-      echo "ERROR: $THISONE ${PAMDIR}/$myfile does not exist, giving up" >&2
+for myfile in ${AUTHFILE} ${SCREENSAVERFILE}; do
+  if [[ ! -f ${PAMDIR}/${myfile} ]]; then
+      echo "ERROR: ${THISONE} ${PAMDIR}/${myfile} doesn't exist, giving up" >&2
       exit 2
   fi
 done
 
-for myfile in $AUTHFILE $SCREENSAVERFILE; do
+for myfile in ${AUTHFILE} ${SCREENSAVERFILE}; do
   # ignore comment lines, then check if PAM already configured for YubiKey
-  if $(grep -v ^[[:blank:]]*# ${PAMDIR}/$myfile | grep -q $YUBILIB 2>/dev/null)
+  if grep -v "^[[:blank:]]*#" ${PAMDIR}/${myfile}\
+  | grep -q ${YUBILIB} 2>/dev/null
   then
-    echo "INFO: $THISONE PAM setup for YubiKey"\
-     "in $myfile looks to already be in place"
+    echo "INFO: ${THISONE} PAM setup for YubiKey"\
+     "in ${myfile} looks to already be in place"
   else
-    echo "INFO: $THISONE adding PAM setup for YubiKey in $myfile"
-    sudo cp -p ${PAMDIR}/$myfile ${PAMDIR}/${myfile}.$DATESTAMP
+    echo "INFO: ${THISONE} adding PAM setup for YubiKey in ${myfile}"
+    sudo cp -p ${PAMDIR}/${myfile} ${PAMDIR}/${myfile}."${DATESTAMP}"
     # enable YubiKey
     # search for first "account required" line and insert a new line before this
     # using a temp file and "-e" to get around newline issue on macOS/POSIX sed
-    mkdir $MYTMPDIR
-    sed -e '/account.*required.*pam_opendirectory.so/i\'$'\n'"$PAMLINE"\
-    ${PAMDIR}/$myfile >${MYTMPDIR}/$myfile
-    if $(sudo cp ${MYTMPDIR}/$myfile ${PAMDIR}/$myfile 2>/dev/null);then
-      sudo chown root ${PAMDIR}/$myfile
-      sudo chgrp wheel ${PAMDIR}/$myfile
-      sudo chmod 644 ${PAMDIR}/$myfile
+    mkdir ${MYTMPDIR}
+    sed -e '/account.*required.*pam_opendirectory.so/i\'$'\n'"${PAMLINE}"\
+    ${PAMDIR}/${myfile} >${MYTMPDIR}/${myfile}
+    if sudo cp ${MYTMPDIR}/${myfile} ${PAMDIR}/${myfile} 2>/dev/null;then
+      sudo chown root ${PAMDIR}/${myfile}
+      sudo chgrp wheel ${PAMDIR}/${myfile}
+      sudo chmod 644 ${PAMDIR}/${myfile}
       changes=$((changes+1))
-      rm ${MYTMPDIR}/$myfile
-      rmdir $MYTMPDIR
+      rm ${MYTMPDIR}/${myfile}
+      rmdir ${MYTMPDIR}
     else 
-      echo "ERROR: $THISONE could not update ${PAMDIR}/$myfile, giving up" >&2
+      echo "ERROR: ${THISONE} couldn't amend ${PAMDIR}/${myfile}, giving up" >&2
       exit 3
     fi
   fi
 done
 
 if [[ $changes -eq 0 ]]; then
-  echo "INFO: $THISONE finished, no changes made to system"
+  echo "INFO: ${THISONE} finished, no changes made to system"
 else
-  echo "INFO: $THISONE PAM setup for YubiKey done, $changes file(s) changed"
+  echo "INFO: ${THISONE} PAM setup for YubiKey done, $changes file(s) changed"
 fi
 
 exit 0
